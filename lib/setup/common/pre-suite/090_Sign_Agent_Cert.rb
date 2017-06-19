335717590da5460ce909f420629a4983c3b1e316
Maggie Dreyer <maggie@puppet.com>
2017-06-19 15:18:54 -0700

    Fix patching and Windows bugs

--- lib/setup/common/pre-suite/090_Sign_Agent_Cert.rb
+++ lib/setup/common/pre-suite/090_Sign_Agent_Cert.rb
@@ -8,7 +8,7 @@ initialize_ssl
 agents.each do |agent|
   step "Add master to agent config and start puppetserver" do
     on(master, facter('fqdn')) do
-      on(agent, "puppet config set --section main server #{stdout.chomp}")
+      on(agent, puppet("config set --section main server #{stdout.chomp}"))
     end
 
     on(master, puppet('resource', 'service', master['puppetservice'], "ensure=running"))
