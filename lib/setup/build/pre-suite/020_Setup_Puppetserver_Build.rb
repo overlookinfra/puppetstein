dcfb7c70b063a13deeb7f8cee7a88e75034d5cb5
Maggie Dreyer <maggie@puppet.com>
2017-06-28 16:40:41 -0700

    Fix mismatched quote

--- lib/setup/build/pre-suite/020_Setup_Puppetserver_Build.rb
+++ lib/setup/build/pre-suite/020_Setup_Puppetserver_Build.rb
@@ -16,7 +16,7 @@ step "Install puppetserver..." do
 
   repo_configs_dir = 'repo-configs'
   install_repos_on(master, 'puppetserver', 'nightly', repo_configs_dir)
-  install_repos_on(master, 'puppet-agent', ENV['PA_SHA'], "repo_configs_dir)
+  install_repos_on(master, 'puppet-agent', ENV['PA_SHA'], repo_configs_dir)
   install_packages_on(master, MASTER_PACKAGES)
 end
 
